7bb52d56006e77c99f95eac1c62a329683e6afd6
Paul Spencer <pauldsp@outlook.com>
2018-06-29 00:30:58 +0100

    Reformatted

--- Header.sh
+++ Header.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-##########
+##################################################
 # HEADER
-##########
+##################################################
 # SCRIPT:		NAME_of_SCRIPT
 # AUTHOR:		Paul Spencer
 # DATE:			DATE_of_CREATION
-# COPYRIGHT:	Copyright (c) Paul Spencer
-# LICENCE:		MIT License
 #
 # PURPOSE: Give a clear, and if necessary, long, description of the
 #          purpose of the shell script. This will also help you stay
 #          focused on the task at hand.
 #
-##########
+##################################################
 # HISTORY
-##########
+##################################################
 #	Date		Author	Description
 # 	2017/05/15	PDS		Script creation
 #
-##########
+##################################################
 # USAGE
-##########
+##################################################
 # Comment out if not needed
 USAGE="This is the script usage"
 
@@ -28,6 +26,6 @@ if [ $# == 0 ] ; then
     echo $USAGE
     exit 1;
 fi
-##########
+##################################################
 # END_OF_HEADER
-##########
+##################################################
